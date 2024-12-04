6a58f43e8b5a832a4c26c0657f74fd2579248f85
Zeel0911 <156478385+Zeel0911@users.noreply.github.com>
2024-12-03 20:30:27 -0500

    Update script.js

---
+++ Assignment-3/js/script.js
@@ -0,0 +1,9 @@
+const apiKey = '911f6a617f6149e49da337468e774278'; // Your News API key
+const newsContainer = document.getElementById('news-container');
+const searchInput = document.getElementById('search');
+const searchBtn = document.getElementById('search-btn');
+const loadingIndicator = document.createElement('p');
+const loadMoreBtn = document.createElement('button');
+const backToTopBtn = document.createElement('button');
+let currentPage = 1; // Track the current page for pagination
+let currentQuery = ''; // Track the current search query
